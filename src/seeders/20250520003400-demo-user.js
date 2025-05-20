5897e95d1db3dd5eb9803cc645cab20e0093378f
uciel <ucibustamante.a@gmail.com>
2025-05-20 09:36:19 -0300

    User Seeder fix

--- src/seeders/20250520003400-demo-user.js
+++ src/seeders/20250520003400-demo-user.js
@@ -55,7 +55,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', {
       email: {
-        [Sequelize.Op.in]: ['juan.perez@example.com', 'laura.gomez@example.com']
+        [Sequelize.Op.in]: ['juan.perez@example.com', 'laura.gomez@example.com', 'admin@example.com']
       }
     }, {});
   }
