a9c11b6837187356f6ed1c65aa4a4194fb8a5e58
nachoji <137451809+nachoji@users.noreply.github.com>
2025-06-03 22:16:45 -0300

    nueva ruta getAllUsers (será search en el futuro)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,9 @@ const { findById, findByEmail } = require("../middlewares/search.middleware");
 const userCtrl = require("../controllers/user.controller");
 const { isRole, verifyToken } = require("../middlewares/authjwt.middleware");
 
+//A ser cambiado en el futuro por search (solo hecho para integrar al front)
+router.get("/", userCtrl.getAllUsers);
+
 router.get("/:id", findById("User"), userCtrl.getUser);
 router.get("/email/:email", findByEmail("User"), userCtrl.getUserByEmail);
 router.post("/", verifyToken, isRole("Admin"), userCtrl.createUser);
