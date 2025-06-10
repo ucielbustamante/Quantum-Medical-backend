413d104c4ff28e33e8fc78117eb67b26708e2c84
nachoji <137451809+nachoji@users.noreply.github.com>
2025-06-10 18:04:48 -0300

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -50,7 +50,9 @@ exports.login = async (req, res) => {
         id: user.id,
         email: user.email,
         role: user.role,
-        accessToken: token
+        accessToken: token,
+        name: user.name,
+        lastname: user.lastname
       }
     });
   } catch (error) {
@@ -104,9 +106,7 @@ exports.register = async (req, res) => {
         id: user.id,
         email: user.email,
         role: user.role,
-        accessToken: token,
-        name: user.name,
-        lastname: user.lastname
+        accessToken: token
       }
     });
   } catch (err) {
