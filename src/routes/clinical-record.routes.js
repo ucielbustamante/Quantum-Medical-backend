3b82392174a3bbb2700a583dbde5afb5f40a96cf
nachoji <137451809+nachoji@users.noreply.github.com>
2025-06-19 17:08:09 -0300

    correcciones

--- src/routes/clinical-record.routes.js
+++ src/routes/clinical-record.routes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const clinicalRecordController = require("../controllers/clinical-record.controller");
 const { verifyToken, isRole } = require("../middlewares/authjwt.middleware");
 const { findById } = require("../middlewares/search.middleware");
+const { authorizeClinicalRecordAccess } = require("../middlewares/authorization.middleware");
 
 router.post("/", verifyToken, isRole("Doctor", "Admin"), clinicalRecordController.createClinicalRecord);
-router.get("/:id", verifyToken, isRole("Patient", "Doctor", "Admin"), findById('ClinicalRecord'), clinicalRecordController.getClinicalRecord);
+router.get("/:id", verifyToken, findById('ClinicalRecord'), clinicalRecordController.getClinicalRecord);
 router.put("/:id", verifyToken, isRole("Doctor", "Admin"), findById('ClinicalRecord'), clinicalRecordController.updateClinicalRecord);
 router.delete("/:id", verifyToken, isRole("Doctor", "Admin"), findById('ClinicalRecord'), clinicalRecordController.deleteClinicalRecord);
-router.get("/", verifyToken, isRole("Patient", "Doctor", "Admin"), clinicalRecordController.searchClinicalRecords);
+router.get("/", verifyToken, authorizeClinicalRecordAccess, clinicalRecordController.searchClinicalRecords);
 
 module.exports = router;
